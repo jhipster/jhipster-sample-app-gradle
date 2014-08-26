39f40fca6b33e1d978b005093e00e8d53cd7ef4b
Julien Dubois <julien.dubois@gmail.com>
2014-08-26 15:13:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+jhipster-sample-app-gradle
+==========================
+
+[![Build Status](https://drone.io/github.com/jhipster/jhipster-sample-app-gradle/status.png)](https://drone.io/github.com/jhipster/jhipster-sample-app-gradle/latest)
+
+This is a sample application created with [JHipster](https://github.com/jdubois/generator-jhipster), using the [Gradle](http://www.gradle.org/) option.
+
+If you want the sample application created with the default settings (Maven instead of Gradle), you can find it [here](https://github.com/jhipster/jhipster-sample-app).
+
+If you want to run the app, run Gradle with the Spring Boot plugin:
+
+```bash
+gradle bootRun
+```
+
+And open [http://localhost:8080/](http://localhost:8080/)
+
+Of course, if you are a true Java hipster, you should use the [Yeoman generator](https://github.com/jdubois/generator-jhipster)!
---
+++ src/test/java/com/mycompany/myapp/service/UserServiceTest.java
@@ -0,0 +1,64 @@
+package com.mycompany.myapp.service;
+
+import com.mycompany.myapp.Application;
+import com.mycompany.myapp.domain.PersistentToken;
+import com.mycompany.myapp.domain.User;
+import com.mycompany.myapp.repository.PersistentTokenRepository;
+import com.mycompany.myapp.repository.UserRepository;
+import org.joda.time.LocalDate;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import javax.inject.Inject;
+
+import static org.assertj.core.api.Assertions.*;
+
+/**
+ * Test class for the UserResource REST controller.
+ *
+ * @see UserService
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+@ActiveProfiles("dev")
+@Transactional
+public class UserServiceTest {
+
+    @Inject
+    private PersistentTokenRepository persistentTokenRepository;
+
+    @Inject
+    private UserRepository userRepository;
+
+    @Inject
+    private UserService userService;
+
+    @Test
+    public void testRemoveOldPersistentTokens() {
+    	User admin = userRepository.findOne("admin");
+    	int existingCount = persistentTokenRepository.findByUser(admin).size();
+        generateUserToken(admin, "1111-1111", new LocalDate());
+        LocalDate now = new LocalDate();
+        generateUserToken(admin, "2222-2222", now.minusDays(32));
+        assertThat(persistentTokenRepository.findByUser(admin)).hasSize(existingCount + 2);
+        userService.removeOldPersistentTokens();
+        assertThat(persistentTokenRepository.findByUser(admin)).hasSize(existingCount + 1);
+    }
+
+    private void generateUserToken(User user, String tokenSeries, LocalDate localDate) {
+        PersistentToken token = new PersistentToken();
+        token.setSeries(tokenSeries);
+        token.setUser(user);
+        token.setTokenValue(tokenSeries + "-data");
+        token.setTokenDate(localDate);
+        token.setIpAddress("127.0.0.1");
+        token.setUserAgent("Test agent");
+        persistentTokenRepository.saveAndFlush(token);
+    }
+}
