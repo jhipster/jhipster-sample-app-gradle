1509adb783c3d56ad3bc219f7e33f989a06af7f8
Julien Dubois <julien.dubois@gmail.com>
2015-08-17 23:48:35 +0200

    automatic project update

--- src/main/webapp/scripts/app/app.constants.js
+++ src/main/webapp/scripts/app/app.constants.js
@@ -4,6 +4,6 @@ angular.module('jhipsterApp')
 
 .constant('ENV', 'dev')
 
-.constant('VERSION', '0.1-SNAPSHOT')
+.constant('VERSION', '0.0.1-SNAPSHOT')
 
 ;
--- src/test/javascript/karma.conf.js
+++ src/test/javascript/karma.conf.js
@@ -51,12 +51,13 @@ module.exports = function (config) {
         reporters: ['dots', 'jenkins', 'coverage', 'progress'],
 
         jenkinsReporter: {
-            outputFile: '../target/test-results/karma/TESTS-results.xml'
+            
+            outputFile: '../build/test-results/karma/TESTS-results.xml'
         },
 
         coverageReporter: {
-            dir: '../target/test-results/coverage',
-
+            
+            dir: '../build/test-results/coverage',
             reporters: [
                 {type: 'lcov', subdir: 'report-lcov'}
             ]
