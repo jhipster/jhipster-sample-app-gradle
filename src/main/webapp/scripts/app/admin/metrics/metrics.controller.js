9096dfac6ddf734abbbd58dd0c62c79631913e4a
Julien Dubois <julien.dubois@gmail.com>
2015-11-20 16:02:38 +0100

    automatic project update

--- src/main/webapp/scripts/app/admin/metrics/metrics.controller.js
+++ src/main/webapp/scripts/app/admin/metrics/metrics.controller.js
@@ -49,7 +49,7 @@ angular.module('sampleGradleApp')
                     size: 'lg',
                     resolve: {
                         threadDump: function() {
-                            return data;
+                            return data.content;
                         }
 
                     }
