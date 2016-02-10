67aa0470de24e0b7d371bc16e9eb36289ad5c772
Julien Dubois <julien.dubois@gmail.com>
2016-02-10 10:24:06 +0100

    automatic project update

--- src/main/webapp/scripts/components/form/pagination.directive.js
+++
@@ -1,9 +0,0 @@
-/* globals $ */
-'use strict';
-
-angular.module('sampleGradleApp')
-    .directive('sampleGradleAppPagination', function() {
-        return {
-            templateUrl: 'scripts/components/form/pagination.html'
-        };
-    });
