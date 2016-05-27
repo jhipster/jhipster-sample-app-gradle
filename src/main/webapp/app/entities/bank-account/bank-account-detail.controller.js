46ba506226465d7bde7aa8ea25b7fc5b4b2b7896
Julien Dubois <julien.dubois@gmail.com>
2016-05-27 11:47:17 +0200

    automatic project update

--- src/main/webapp/app/entities/bank-account/bank-account-detail.controller.js
+++ src/main/webapp/app/entities/bank-account/bank-account-detail.controller.js
@@ -9,12 +9,12 @@
 
     function BankAccountDetailController($scope, $rootScope, $stateParams, entity, BankAccount, User, Operation) {
         var vm = this;
+
         vm.bankAccount = entity;
-        
+
         var unsubscribe = $rootScope.$on('jhipsterGradleSampleApplicationApp:bankAccountUpdate', function(event, result) {
             vm.bankAccount = result;
         });
         $scope.$on('$destroy', unsubscribe);
-
     }
 })();
--- src/main/webapp/app/services/auth/has-authority.directive.js
+++ src/main/webapp/app/services/auth/has-authority.directive.js
@@ -5,6 +5,8 @@
         .module('jhipsterGradleSampleApplicationApp')
         .directive('hasAuthority', hasAuthority);
 
+    hasAuthority.$inject = ['Principal'];
+
     function hasAuthority(Principal) {
         var directive = {
             restrict: 'A',
