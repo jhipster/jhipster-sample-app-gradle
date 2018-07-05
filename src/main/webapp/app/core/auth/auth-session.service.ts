2cf387070ab76c8452c12b151ca30893474d479c
Julien Dubois <julien.dubois@gmail.com>
2018-07-05 11:29:51 +0200

    automatic project update

--- src/main/webapp/app/core/auth/auth-session.service.ts
+++ src/main/webapp/app/core/auth/auth-session.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { SERVER_API_URL } from 'app/app.constants';
 
@@ -24,10 +25,12 @@ export class AuthServerProvider {
 
     logout(): Observable<any> {
         // logout from the server
-        return this.http.post(SERVER_API_URL + 'api/logout', {}, { observe: 'response' }).map((response: HttpResponse<any>) => {
-            // to get a new csrf token call the api
-            this.http.get(SERVER_API_URL + 'api/account').subscribe(() => {}, () => {});
-            return response;
-        });
+        return this.http.post(SERVER_API_URL + 'api/logout', {}, { observe: 'response' }).pipe(
+            map((response: HttpResponse<any>) => {
+                // to get a new csrf token call the api
+                this.http.get(SERVER_API_URL + 'api/account').subscribe(() => {}, () => {});
+                return response;
+            })
+        );
     }
 }
